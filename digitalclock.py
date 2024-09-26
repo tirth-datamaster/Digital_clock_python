395c3bd59ce3ed50483ac2959466d22fb9911812
TIRTH PATEL <tirth15042006@gmail.com>
2024-09-26 06:55:37 +0530

    Add files via upload

---
+++ digitalclock.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from time import strftime
+
+root = tk.Tk()
+root.title("DIGITAL CLOCK MADE BY TIRTH")
+
+
+def time():
+    string = strftime("%H:%M:%S %p \n %D")
+    label.config(text = string)
+    label.after(1000,time)
+
+label = tk.Label(root,font=("Calibri",50,"bold"),background="black",foreground="white")
+label.pack(anchor="center")
+
+time()
+
+label.mainloop()
